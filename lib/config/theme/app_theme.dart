e7ba2e35f051a47c8c4373fdff680cea0bac808a
Raul de Arriba <rauldearriba@gmail.com>
2024-02-22 05:54:36 +0100

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5C11D4);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.red,
+  Colors.pink,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  }) : assert(selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+            'Colors must be between 0 and ${_colorThemes.length}');
+
+  ThemeData theme() {
+    return ThemeData(
+        useMaterial3: true, colorSchemeSeed: _colorThemes[selectedColor]);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/chat/chat_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 6).theme(),
+        home: const ChatScreen());
+  }
+}
